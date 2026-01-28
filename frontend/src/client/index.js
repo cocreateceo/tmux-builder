914caae6a8d2ba5106d9c582b1e67248bc0aa574
Gopinath Varadharajan <gopi@sunwaretechnologies.com>
2026-01-28 11:54:05 -0800

    chore(client): add index exports

---
+++ frontend/src/client/index.js
@@ -0,0 +1,5 @@
+export { default as ClientApp } from './ClientApp';
+export { ThemeProvider, useThemeContext } from './context/ThemeContext';
+export { useTheme } from './hooks/useTheme';
+export { useClientSession } from './hooks/useClientSession';
+export { clientApi } from './services/clientApi';
